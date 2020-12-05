35bc995a9b6f803a200ce14bd79e04f8253d2b0f
dbrennand <52419383+dbrennand@users.noreply.github.com>
2020-12-05 16:27:37 +0000

    Add helper function `get_server_id`. Cleanup code for playerinfo command. Still WIP.

--- Nanite/planetside2.py
+++ Nanite/planetside2.py
@@ -24,6 +24,27 @@ class Planetside2(commands.Bot):
 
     # Helpers functions
 
+    async def get_server_id(self, server_name: str) -> int:
+        """
+        Retrieve the ID for a Planetside 2 server.
+
+        [API documentation](https://ps2.fisu.pw/api/territory/)
+
+        :param server_name: The name of the Planetside 2 server to obtain the ID for.
+        :returns: A int representing the ID of the Planetside 2 server if found, otherwise, returns None.
+        """
+        # Declare server dict
+        servers = {
+            "connery": 1,
+            "miller": 10,
+            "cobalt": 13,
+            "emerald": 17,
+            "jaeger": 19,
+            "apex": 24,
+            "solTech": 40,
+        }
+        return servers.get(server_name.lower(), None)
+
     async def get_json(self, session: aiohttp.ClientSession, endpoint: str) -> dict:
         """
         Retrieve JSON from a specified API endpoint.
@@ -51,33 +72,25 @@ class Planetside2(commands.Bot):
 
         :param server: The name of the server to retrieve population data for.
         """
-        servers = {
-            "connery": 1,
-            "miller": 10,
-            "cobalt": 13,
-            "emerald": 17,
-            "jaeger": 19,
-            "apex": 24,
-            "solTech": 40,
-        }
-        capital_server = server.capitalize()
-        choice = servers.get(server.lower(), None)
-        if choice:
+        # Obtain server ID
+        server_id = await self.get_server_id(server)
+        if server_id:
             async with aiohttp.ClientSession() as session:
                 try:
                     json = await self.get_json(
-                        session, f"https://ps2.fisu.pw/api/population/?world={choice}"
+                        session,
+                        f"https://ps2.fisu.pw/api/population/?world={server_id}",
                     )
                     results = json["result"][0]
                     embed = discord.Embed(
-                        title=f"Current population for {capital_server}:",
+                        title=f"Current population for {server.capitalize()}:",
                         colour=discord.Colour.teal(),
                         description=f"**Terran Republic**: {results['tr']}\n**New Conglomerate**: {results['nc']}\n**Vanu Sovereignty**: {results['vs']}\n**Nanite Systems Operative**: {results['ns']}",
                     )
                     await ctx.send(embed=embed)
-                except aiohttp.ClientResponseError as err:
+                except: # TODO: Find out what specific error occurs here
                     logger.debug(
-                        f"An error occurred collecting population data for {server}: {err}."
+                        f"An error occurred collecting population data for {server}."
                     )
                     await ctx.send(
                         f"An error occurred retrieveing population data for {server}."
@@ -95,18 +108,30 @@ class Planetside2(commands.Bot):
         """
         async with auraxium.Client() as client:
             try:
+                # Obtain general player info
                 player = await client.get_by_name(auraxium.ps2.Character, player_name)
             except ValueError as err:
-                logger.debug(f"An error occurred looking up player {player_name}: {err}. Most likely they don't exist.")
+                logger.debug(
+                    f"An error occurred looking up player {player_name}: {err}. Most likely they don't exist."
+                )
                 ctx.send(f"Player {player_name} not found.")
-                break
+                return
+            # Obtain player faction info
             player_faction = await player.faction()
+            # Build faction image URL
+            faction_image_url = (
+                f"https://census.daybreakgames.com{player_faction.data.image_path}"
+            )
+            # Obtain player outfit info
             player_outfit = await player.outfit()
+            # Obtain player online status info
             player_online = await player.is_online()
+            # Change colour of discord embed depending wheather the player is online or not
             if player_online:
                 colour = discord.Colour.green()
             else:
                 colour = discord.Colour.red()
+            # Build embed message containing player info
             player_stats = f"""**Faction**: {player_faction}
 **Outfit**: {player_outfit}
 **Currently playing**: {player_online}
@@ -117,7 +142,13 @@ class Planetside2(commands.Bot):
 **Minutes played**: {player.data.times.minutes_played}
 **Certs earned**: {player.data.certs.earned_points}
             """
-            embed = discord.Embed(title=f"{player.data.name.first}'s Stats:", description=player_stats, colour=colour)
+            # Build embed and send
+            embed = discord.Embed(
+                title=f"{player.data.name.first}'s Stats:",
+                description=player_stats,
+                colour=colour,
+            ).set_thumbnail(url=faction_image_url)
+            await ctx.send(embed=embed)
 
     @commands.command()
     async def outfitstats(self, ctx: commands.Context, outfit: str = "redmist"):
