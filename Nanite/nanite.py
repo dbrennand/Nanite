74b1d97e12af3a433be9657323c1643223836579
dbrennand <52419383+dbrennand@users.noreply.github.com>
2021-05-03 18:44:21 +0100

    refactor: Alter docstring and imports.

--- Nanite/nanite.py
+++ Nanite/nanite.py
@@ -1,36 +1,37 @@
-# Settings imports
-from .config import BOT_COMMAND_PREFIX, BOT_DESCRIPTION, BOT_STATUS
+"""Entrypoint file for Nanite.
+"""
+# Config imports
+from .config import BOT_COMMAND_PREFIX, BOT_DESCRIPTION
 
-# Bot imports
+# Library imports
 import discord
-from discord.ext import commands
+import datetime
 from loguru import logger
-from datetime import datetime
 
-nanite = commands.Bot(BOT_COMMAND_PREFIX, description=BOT_DESCRIPTION)
+nanite = discord.ext.commands.Bot(BOT_COMMAND_PREFIX, description=BOT_DESCRIPTION)
 
 # Events and commands
 
 
 @nanite.event()
-async def on_ready():
+async def on_ready() -> None:
     """
-    Runs when the the bot has successfully connected to the Discord API.
+    Log an INFO message when the bot has successfully connected to the Discord API.
     """
-    logger.debug(
-        f"Successfully connected to the Discord API. Connected as user: {nanite.user}."
+    logger.info(
+        f"Successfully connected to the Discord API. Connected as user: {nanite.user}"
     )
 
 
 @nanite.command()
-async def ping(ctx: commands.Context):
-    """
-    Pongs back to check the bot is running and the response time in milliseconds.
+async def ping(ctx: discord.ext.commands.Context) -> None:
+    """Pongs back to check the bot is running. Returns the bot's response time in milliseconds.
 
-    :param ctx: Holds the context in which a command is invoked under.
+    Args:
+        ctx (discord.ext.commands.Context): Represents the context in which a command is being invoked under.
     """
-    delta = datetime.now() - ctx.message.created_at
-    logger.debug(f"Running ping command.\nTime difference: {time_difference}")
+    delta = datetime.datetime.now() - ctx.message.created_at
+    logger.info(f"Running ping command. Time difference: {time_difference}")
     await ctx.send(
         f"Pong!\n**Latency**: {delta.total_seconds() * 1000}ms",
         embed=discord.Embed(title=":ping_pong:", colour=discord.Colour.red()),
